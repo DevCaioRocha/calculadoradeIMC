d3ec990101942580efbf3d9ad4a76f85c103e207
Caio Rocha <dev.caiorocha@gmail.com>
2023-01-09 21:39:26 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const peso = document.getElementById("peso");
+const altura = document.getElementById("altura");
+
+function calcularIMC() {
+  return peso * altura;
+}
+
+calcularimc()
+
+/* let IMC = "";
+
+if(IMC > 19 || IMC < 24){
+console.log("Peso Ideal");
+}else {
+console.log("pesado");
+} */
